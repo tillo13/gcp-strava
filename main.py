71bcbb8bd7788af49480bf64275270ba7f2e4bc4
Andy Tillo <andy.tillo@gmail.com>
2023-07-08 18:03:48 -0700

    successful google postgres post with creds

--- main.py
+++ main.py
@@ -1,28 +1,53 @@
-#TODO_LIST: 
-#1. secure the credentials in google secret DONE, note had to add Secret manager secret accessor here: https://console.cloud.google.com/iam-admin/iam?project=gcp-strava
-#2. save to DB instead of CSV, better output messages.
-#3. fix WARNING:  Python 3.5-3.7 will be deprecated on August 8th, 2023. Please use Python version 3.8 and up.
-#4  handle the case when the 'code' parameter is not in the request. You might want to send a response indicating that the request is not as expected. 
 from flask import Flask, request, redirect
 from google.cloud import secretmanager
 import os
 import requests
 import csv
+import psycopg2
+from psycopg2 import OperationalError
+import socket
+import sqlalchemy
+from sqlalchemy import create_engine, text
+import logging
+import time
+
+logging.basicConfig()
+logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 
 app = Flask(__name__)
 
-# Function to get your Strava Client Secret
 def get_secret_version(project_id, secret_id, version_id="latest"):
     client = secretmanager.SecretManagerServiceClient()
     name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
     response = client.access_secret_version(name=name)
     return response.payload.data.decode('UTF-8')
 
-# Use your actual project id, client id, and redirect URL
-SECRET_MANAGER_PROJECT_ID = "97418787038"
+GCP_PROJECT_ID = "97418787038"
 STRAVA_CLIENT_ID = "110278"
 REDIRECT_URL = "https://gcp-strava.wl.r.appspot.com/exchange_token"
-STRAVA_CLIENT_SECRET = get_secret_version(SECRET_MANAGER_PROJECT_ID, "strava_client_secret")
+STRAVA_API_SECRET = "strava_client_secret"
+STRAVA_CLIENT_SECRET = get_secret_version(GCP_PROJECT_ID, STRAVA_API_SECRET)
+
+#get Google Secret DB
+GOOGLE_SECRET_DB_ID = "gcp_strava_db_password"
+DB_PASSWORD = get_secret_version(GCP_PROJECT_ID, GOOGLE_SECRET_DB_ID)
+
+def create_conn():
+    db_user = 'postgres'
+    db_pass = DB_PASSWORD
+    db_name = 'gcp_strava_data' 
+    cloud_sql_connection_name = 'gcp-strava:us-central1:gcp-default'
+
+    engine = sqlalchemy.create_engine(
+        sqlalchemy.engine.url.URL.create(
+            drivername="postgresql+psycopg2",
+            username=db_user,
+            password=db_pass,
+            database=db_name,
+            host=f'/cloudsql/{cloud_sql_connection_name}',
+        ),
+    )
+    return engine
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -30,6 +55,8 @@ def home():
 
 @app.route('/login', methods=['GET'])
 def login():
+    global start_time
+    start_time = time.time()
     params = {
         "client_id": STRAVA_CLIENT_ID,
         "response_type": "code",
@@ -44,42 +71,71 @@ def login():
 @app.route('/exchange_token', methods=['GET'])
 def exchange_token():
     code = request.args.get('code')
+    messages = []
     if code:
-        url = "https://www.strava.com/oauth/token"
-
-        payload = {
-            'client_id': STRAVA_CLIENT_ID,
-            'client_secret': STRAVA_CLIENT_SECRET,
-            'code': code,
-            'grant_type': 'authorization_code'
-        }
-
         try:
+            # Build the payload to get the token
+            url = "https://www.strava.com/oauth/token"
+            payload = {
+                'client_id': STRAVA_CLIENT_ID,
+                'client_secret': STRAVA_CLIENT_SECRET,
+                'code': code,
+                'grant_type': 'authorization_code'
+            }
+            # Attempt to get token
             response = requests.post(url, params=payload)
             response.raise_for_status()
-
             data = response.json()
 
+            messages.append('1. Connect to Strava authorize (strava.com/oauth/authorize): Success!') 
+            
+            # Get tokens
             access_token = data['access_token']
             refresh_token = data['refresh_token']
             expires_at = data['expires_at']
+            athlete_id = data['athlete']['id']
+            expires_in = data['expires_in']
 
-            # Write the tokens into a temporary CSV file in the local file system
-            with open('/tmp/tokens.csv', 'a', newline='') as file:
-                writer = csv.DictWriter(file, fieldnames=['client_id', 'access_token', 'expires_at', 'refresh_token'])
-                writer.writerow({
-                    'client_id': STRAVA_CLIENT_ID,
-                    'access_token': access_token,
-                    'expires_at': expires_at,
-                    'refresh_token': refresh_token
-                })
+            messages.append('2. Token Exchange (strava.com/oauth/token): Success!')
+            messages.append(f'3. Athlete_ID ({athlete_id}): Success!')
+            messages.append(f'4. Token expires: {expires_at}')
+            messages.append(f'5. Access Token: {access_token}')
+            messages.append(f'6. Refresh Token: {refresh_token}')
 
-            return f"All done! Your tokens are stored in tokens.csv file."
+            # Attempt to save to database
+            messages.append('7. Save to Database: Attempting...')
+            engine = create_conn()
+            with engine.begin() as connection:
+                result = connection.execute(
+                    text("""
+                    INSERT INTO strava_access_tokens (client_id, athlete_id, access_token, refresh_token, expires_at, expires_in)
+                    VALUES (:client_id, :athlete_id, :access_token, :refresh_token, :expires_at, :expires_in)
+                    RETURNING pk_id
+                    """),
+                    {
+                        "client_id": STRAVA_CLIENT_ID,
+                        "athlete_id": athlete_id,
+                        "access_token": access_token,
+                        "refresh_token": refresh_token,
+                        "expires_at": expires_at,
+                        "expires_in": expires_in
+                    }
+                )
+                pk_id = result.fetchone()[0]
+            messages.append(f'8. Save to Database: Success! pk_id = {pk_id}')
 
         except requests.exceptions.RequestException as err:
-            return str(err)
-    else:
-        return "No code received"
+            messages.append(f'Request Failed: {err}')
+        
+        except Exception as e:
+            messages.append(f'Unexpected Error: {e}')
+        
+        finally:
+            roundtrip_time = time.time() - start_time
+            messages.append(f'From login to now: {roundtrip_time} seconds.')
+            messages.append('<br><a href="https://gcp-strava.wl.r.appspot.com/">Try again?</a>')
+            return '<br>'.join(messages)
+    return 'Uh oh, an error occurred when trying to get the token.'
 
 if __name__ == "__main__":
     app.run(debug=True)
